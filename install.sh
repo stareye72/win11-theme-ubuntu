73302feb0901c80f925d26a693da1053159cf5fd
Menuhin Saitov <stareye72@ymail.com>
2025-12-24 00:04:09 +0100

    Creating .icons and .themes and moved the Dir

--- install.sh
+++ install.sh
@@ -23,5 +23,25 @@ tar -xvf Win11-round-Light-compact.tar
 #Cloning dashtopanel from Git Repository
 git clone https://github.com/home-sweet-gnome/dash-to-panel.git
 
+#Creating the .icons and .theme directorys
 
+if [ -d $HOME/.icons]; then
+	mv Windows-Eleven $HOME/.icons
+else
+	mkdir -p $HOME/.icons
+	mv Windows-Eleven $HOME/.icons
+fi
 
+if [ -d $HOME/.themes]; then
+	mv Win11-round-Light-compact $HOME/.themes
+else
+	mkdir-p $HOME/.themes
+	mv Win11-round-Light-compact $HOME/.themes
+fi
+
+if [ -d $HOME/.icons]; then
+	mv Win11OS $HOME/.icons
+else
+	mkdir -p $HOME/.icons
+	mv Win11OS $HOME/.icons
+fi
