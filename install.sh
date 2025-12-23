243e52c3340caf49b100f8340a3125f36c286b7b
Menuhin Saitov <stareye72@ymail.com>
2025-12-23 22:33:22 +0100

    Install Script added

---
+++ install.sh
@@ -0,0 +1,16 @@
+#Install Scipt for Windows Eleven Theme for Ubuntu
+
+#Extracting the Mousepointer
+unzip Win11OS.zip
+
+#Extracing the Icons
+unxz Windows-Eleven.tar.xz
+tar -xvf Windows-Eleven.tar
+
+#Extracting the Wallpaper
+unxz windows-eleven.tar.xz
+tar -xvf windows-eleven.tar
+
+
+
+
